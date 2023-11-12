0f232459da36ebf56cd9aa6af114bed3bfd8de39
Marcus Antunius <marcusantunius@gmail.com>
2023-11-12 15:36:06 -0300

    Revert "add any json object"
    This reverts commit 3f373a87f9cc3db18a21531d840b73f4e30e612c.

--- src/main/kotlin/com/marcus/restaurant/controller/RestaurantController.kt
+++ src/main/kotlin/com/marcus/restaurant/controller/RestaurantController.kt
@@ -1,13 +1,10 @@
 package com.marcus.restaurant.controller
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import org.springframework.core.io.ClassPathResource
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-
 
 @RequestMapping("restaurant")
 @RestController
@@ -19,9 +16,7 @@ class RestaurantController {
     }
 
     @PostMapping("/order")
-    fun receiveOrder(@RequestBody order: Order): Welcome? {
-        val resource = ClassPathResource("static/response.json").inputStream
-        val objectMapper = jacksonObjectMapper()
-        return objectMapper.readValue(resource, Welcome::class.java)
+    fun receiveOrder(@RequestBody order: Order): String {
+        return "Received ${order.id}"
     }
 }
