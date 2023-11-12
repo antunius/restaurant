3f373a87f9cc3db18a21531d840b73f4e30e612c
Marcus Antunius <marcusantunius@gmail.com>
2023-11-12 10:54:16 -0300

    add any json object

--- src/main/kotlin/com/marcus/restaurant/controller/RestaurantController.kt
+++ src/main/kotlin/com/marcus/restaurant/controller/RestaurantController.kt
@@ -1,10 +1,13 @@
 package com.marcus.restaurant.controller
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.core.io.ClassPathResource
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+
 
 @RequestMapping("restaurant")
 @RestController
@@ -16,7 +19,9 @@ class RestaurantController {
     }
 
     @PostMapping("/order")
-    fun receiveOrder(@RequestBody order: Order): String {
-        return "Received ${order.id}"
+    fun receiveOrder(@RequestBody order: Order): Welcome? {
+        val resource = ClassPathResource("static/response.json").inputStream
+        val objectMapper = jacksonObjectMapper()
+        return objectMapper.readValue(resource, Welcome::class.java)
     }
 }
