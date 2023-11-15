6da34d64d65afcaf3a83d303d27b0634b7538087
Marcus Antunius <marcusantunius@gmail.com>
2023-11-15 15:27:07 -0300

    json

--- src/main/kotlin/com/marcus/restaurant/controller/RestaurantController.kt
+++ src/main/kotlin/com/marcus/restaurant/controller/RestaurantController.kt
@@ -1,5 +1,8 @@
 package com.marcus.restaurant.controller
 
+import Response
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -16,7 +19,5 @@ class RestaurantController {
     }
 
     @PostMapping("/order")
-    fun receiveOrder(@RequestBody order: Order): String {
-        return "Received ${order.id}"
-    }
+    fun receiveOrder(@RequestBody order: Order) = Response.get()
 }
